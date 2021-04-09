4ca39a981a45ec7316f2e332cafa5d3d1593e932
Piyush Kumar <44132300+kpiyush04@users.noreply.github.com>
2021-04-09 17:34:29 +0530

    Update test_task-2.R

--- medium/test_task-2.R
+++ medium/test_task-2.R
@@ -8,7 +8,7 @@ test_that("desired_bugissue works", {
   # characterizing a base URL that will be linked with bug_id to divert to the program
   base_url <- "https://bugs.r-project.org/bugzilla/show_bug.cgi?id="
   
-  # taking a for loop form 0 to 7 and 
+  # taking a for loop from 0 to 7 and 
   for (i in 0:7) {
     # calling the desired_bug function from 0 to 7 which is taking two formal augments i.e. base_url and considering i as bug_id
     t1 <- desired_bug(base_url, i) 
